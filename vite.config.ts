d327d2d1d93d0fc2a25a68d3c2c1442143205f6f
Jérémy LAMOUR <jeremy.lamour@student.junia.com>
2025-01-13 19:49:35 +0100

    modif redirect

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react'], // Exclusion d'une dépendance spécifique si nécessaire
+  },
+  server: {
+    fs: {
+      strict: true, // Option recommandée pour plus de sécurité
+    },
+    open: true, // Ouvre le navigateur automatiquement lors du démarrage du serveur
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
   },
 });
